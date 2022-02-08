a7e7424ca8a1682054a698747cb354d195dea81b
lentyss <elenatur03@outlook.com>
2022-02-08 14:00:17 +0300

    Исправление ошибок 1

--- js/storage.js
+++ js/storage.js
@@ -1,21 +1,21 @@
 function setStorage(key, value) {
-    if (value != 1000)
-        //  проверки на null не хватает
+    if (value != 1000 && key !== null)
+        //  проверка на null у меня не работает
         localStorage[key] = value;
     return key;
 }
 function getStorage(key) {
     // Здесь надо получать по названию ключа, а не по числу статическому
-    return localStorage.getItem('1');
+    return localStorage.getItem(key);
 }
 function update(){
     // эта функция не должна обращаться к стору напрямую, а должна использовать вышестоящии функции.
-    localStorage['2'] = Number(localStorage.getItem('2')) + 1;
-    return localStorage.getItem('2');
+    localStorage['2'] = Number(getStorage('2')) + 1;
+    return getStorage('2');
 }
 // нет, надо обращаться к setStorage()
-per = Number(localStorage.getItem('2'));
 console.log(getStorage(setStorage('1', 'money')));
-getStorage(setStorage('2', 1000));
+per = Number(getStorage(setStorage('2', 1000)));
 // ну личтнер здесь лишний, просто вызывать update() и всё
+// не знаю, как еще отловить событие F5
 document.addEventListener("DOMContentLoaded", console.log(update()));
